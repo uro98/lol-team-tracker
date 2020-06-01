0a556c3d60fcd92a4dddc843580f8f96fc10b3ef
Yu-Jo Tseng <zoey5538@gmail.com>
2020-06-02 00:04:45 +0100

    Reverse function order and set account id as an arg

--- tracker.py
+++ tracker.py
@@ -4,15 +4,29 @@ import time
 summoner_name = 'Joppiesaus'
 region = 'euw1'
 api_key = 'RGAPI-c81d321a-e0c0-4e0b-87d0-23f00ce14576'
-account_id = None
 last_timestamp = None
 rate_limit = 1.201
 
-def get(url):
-    response = requests.get(url)
-    json_response = response.json()
-    time.sleep(rate_limit)
-    return json_response
+def main():
+    if last_timestamp:
+        print('')
+    else:
+        summoner = get(f'https://{region}.api.riotgames.com/lol/summoner/v4/summoners/by-name/{summoner_name}?api_key={api_key}')
+        process_last_hundred_games(summoner['accountId'])
+
+def process_last_hundred_games(account_id):
+    matchlist = get(f'https://{region}.api.riotgames.com/lol/match/v4/matchlists/by-account/{account_id}?api_key={api_key}')
+    matches = matchlist['matches']
+    for match in matches:
+        process_match(match)
+
+def process_match(match):
+    game_id = match['gameId']
+    match_details = get(f'https://{region}.api.riotgames.com/lol/match/v4/matches/{game_id}?api_key={api_key}')
+    
+    players = match_details['participantIdentities']
+    for player in players:
+        process_player(player)
 
 def process_player(player):
     summoner_id = player['player']['summonerId']
@@ -27,27 +41,11 @@ def process_player(player):
             lp = league['leaguePoints']
             print(f'{summoner_name}: {tier} {rank} {lp}lp')
 
-def process_match(match):
-    game_id = match['gameId']
-    match_details = get(f'https://{region}.api.riotgames.com/lol/match/v4/matches/{game_id}?api_key={api_key}')
-    
-    players = match_details['participantIdentities']
-    for player in players:
-        process_player(player)
-
-def process_last_hundred_games():
-    matchlist = get(f'https://{region}.api.riotgames.com/lol/match/v4/matchlists/by-account/{account_id}?api_key={api_key}')
-    matches = matchlist['matches']
-    for match in matches:
-        process_match(match)
-
-def main():
-    if last_timestamp:
-        print('')
-    else:
-        summoner = get(f'https://{region}.api.riotgames.com/lol/summoner/v4/summoners/by-name/{summoner_name}?api_key={api_key}')
-        account_id = summoner['accountId']
-        process_last_hundred_games()
+def get(url):
+    response = requests.get(url)
+    json_response = response.json()
+    time.sleep(rate_limit)
+    return json_response
 
 if __name__ == '__main__':
     main()
